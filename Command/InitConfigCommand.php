f4047b9b317382505caf28e88dd24286670f65b9
c33s <dev@c33s.net>
2014-01-15 18:38:15 +0100

    init config wip

--- Command/InitConfigCommand.php
+++ Command/InitConfigCommand.php
@@ -6,16 +6,18 @@ namespace c33s\CoreBundle\Command;
 //
 //use Symfony\Component\Console\Command\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
+//use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Process\Process;
+//use Symfony\Component\Console\Input\ArrayInput;
+//use Symfony\Component\Process\Process;
 
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOException;
+//use Symfony\Component\Filesystem\Exception\IOException;
+
+use c33s\CoreBundle\Util\BundleHelper;
 
 class InitConfigCommand extends ContainerAwareCommand
 {
@@ -36,20 +38,29 @@ class InitConfigCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-	//$output->writeln('starting dumping config files');
 	$output->writeln('<info>c33s:</info>');
 	
-	//$templateing = $this->getContainer()->get('templating');
+	//$this->copyConfigs();
+	$this->addBundles();
+    }
+    
+    protected function addBundles()
+    {
+	echo "foo";
+	$kernel = $this->getContainer()->get('kernel');
+	$bundleHelper = new BundleHelper($kernel);
+    }
+
+
+    protected function copyConfigs()
+    {
 	$kernel = $this->getContainer()->get('kernel');
 	$sourcePath = $kernel->locateResource('@c33sCoreBundle/Resources/templates/config');
 	$targetPath = $this->getContainer()->getParameter('kernel.root_dir').'/config';
-	//var_dump($sourcePath,$targetPath);
-	
 
 	$fs = new Filesystem();
-	//var_dump($name = $input->getOption('force'));
 	$overwrite = $input->getOption('force');
 	$options = array('override' => $overwrite);
-	$fs->mirror($sourcePath, $targetPath, null, $options);
+	$fs->mirror($sourcePath, $targetPath, null, $options);	
     }
 }
