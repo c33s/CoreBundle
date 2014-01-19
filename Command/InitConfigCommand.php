1d24d7fcff24b96087ec932e6d32439596693f82
c33s <dev@c33s.net>
2014-01-19 22:16:18 +0100

    inti config command wip

--- Command/InitConfigCommand.php
+++ Command/InitConfigCommand.php
@@ -14,19 +14,21 @@ use Symfony\Component\Console\Output\OutputInterface;
 //use Symfony\Component\Console\Input\ArrayInput;
 //use Symfony\Component\Process\Process;
 
-use Symfony\Component\Filesystem\Filesystem;
+//use Symfony\Component\Filesystem\Filesystem;
 //use Symfony\Component\Filesystem\Exception\IOException;
 
 use c33s\CoreBundle\Util\BundleHelper;
 
 class InitConfigCommand extends ContainerAwareCommand
 {
-
+    protected $input;
+    protected $output;
+    
     protected function configure()
     {
         $this
-            ->setName('c33s:initconfig')
-            ->setDescription('the task will init the project by coping the template config files (config/assettic.yml, propel.yml,...) to the fresh project. do not call this command if you have allready set up your project.')
+            ->setName('c33s:init-config')
+            ->setDescription('the task will init the projects config with the importing system')
 	    ->addOption(
                'force',
                null,
@@ -38,29 +40,45 @@ class InitConfigCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-	$output->writeln('<info>c33s:</info>');
-	
+	$output->writeln('<info>c33s:init-config</info>');
+        $this->input = $input;
+        $this->output = $output;
 	//$this->copyConfigs();
 	$this->addBundles();
+        //var_dump();
+    }
+    
+    protected function addImporterToConfig()
+    {
+        $configFile = $this->getConfigYmlPath();
+        $configToAdd = "    - { resource: @c33sCoreBundle/Resources/config/config/_importer.yml }\n";
+        $stringAfterToInsert = "- { resource: @c33sCoreBundle/Resources/config/config.yml }\n";
+        
+        Tools::addLineToFile($configFile,$configToAdd,$stringAfterToInsert);
+        
+        $this->output->writeln('added CoreBundle config.yml to imports');
     }
     
     protected function addBundles()
     {
-	echo "foo";
-	$kernel = $this->getContainer()->get('kernel');
-	$bundleHelper = new BundleHelper($kernel);
+        $bundles = $this->getContainer()->parameters['c33s_core.config.bundles'];
+        foreach ($bundles as $bundle)
+        {
+            echo $bundle."\n";
+        }
+        $this->output->writeln('added Bundles');
     }
 
 
-    protected function copyConfigs()
-    {
-	$kernel = $this->getContainer()->get('kernel');
-	$sourcePath = $kernel->locateResource('@c33sCoreBundle/Resources/templates/config');
-	$targetPath = $this->getContainer()->getParameter('kernel.root_dir').'/config';
-
-	$fs = new Filesystem();
-	$overwrite = $input->getOption('force');
-	$options = array('override' => $overwrite);
-	$fs->mirror($sourcePath, $targetPath, null, $options);	
-    }
+//    protected function copyConfigs()
+//    {
+//	$kernel = $this->getContainer()->get('kernel');
+//	$sourcePath = $kernel->locateResource('@c33sCoreBundle/Resources/templates/config');
+//	$targetPath = $this->getContainer()->getParameter('kernel.root_dir').'/config';
+//
+//	$fs = new Filesystem();
+//	$overwrite = $input->getOption('force');
+//	$options = array('override' => $overwrite);
+//	$fs->mirror($sourcePath, $targetPath, null, $options);	
+//    }
 }
