d7e8ff3bfcd2cc5fcabd04f87f7655ef446ae6ee
c33s <dev@c33s.net>
2014-04-25 18:46:40 +0200

    added NameHelper

---
+++ Helper/NameHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace c33s\CoreBundle\Helper;
+
+class NameHelper
+{
+    protected $name;
+    protected $nameCamelcase;
+    protected $nameUnderscore;
+    
+    public function __construct($name,$inflector)
+    {
+	$this->name = ucfirst($name);
+	$this->nameCamelcase = $inflector->camelize($name);
+	$this->nameUnderscore = $inflector->underscore($name);
+    }
+    
+    public function getName()
+    {
+	return $this->name;
+    }
+    
+    public function camelcased()
+    {
+	return $this->nameCamelcase;
+    }
+
+    public function underscored()
+    {
+	return $this->nameUnderscore;
+    }
+
+    public function __toString()
+    {
+	return $this->name;
+    }
+}
