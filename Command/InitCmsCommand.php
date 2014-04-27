d011e4b2a2c5bb62bdb5d1bb88e40f1b816b8330
c33s <dev@c33s.net>
2014-04-27 22:01:20 +0200

    InitCmsCommand wip

--- Command/InitCmsCommand.php
+++ Command/InitCmsCommand.php
@@ -13,6 +13,7 @@ use Symfony\Component\Filesystem\Filesystem;
 
 //use c33s\CoreBundle\Tools\Tools;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Finder\Finder;
 //use c33s\CoreBundle\Util\AkelosInflector as Inflector;
 
 //use Composer\IO\ConsoleIO;
@@ -24,7 +25,9 @@ class InitCmsCommand extends ContainerAwareCommand
 {
     protected $io;
     protected $fs;
+    protected $finder;
     protected $bundles;
+    protected $asseticBundles;
     protected $name;
     //protected $input;
     //protected $output;
@@ -54,6 +57,13 @@ class InitCmsCommand extends ContainerAwareCommand
                'supply an array of bundle names which should be generated',
 		array('Webpage', 'Admin', 'AdminGen', 'Model')
             )
+	    ->addOption(
+               'assetic-bundles',
+               null,
+               InputOption::VALUE_IS_ARRAY | InputArgument::OPTIONAL,
+               'supply an array of bundle names which should be generated',
+		array('Webpage', 'Admin', 'AdminGen')
+            )
         ;
     }
 
@@ -64,44 +74,64 @@ class InitCmsCommand extends ContainerAwareCommand
 	$this->fs = new Filesystem();
 	
 	$this->bundles = $input->getOption('bundles');
+	$this->asseticBundles = $input->getOption('bundles');
 	$this->initNameHelper($input->getArgument('name'));
 	
 	if (!$input->getOption('no-bundles'))
 	{
 	    $this->generatePredifinedBundles();
 	}
-	$this->generateRouting();
 	
-	$this->initStaticPageController();
 	$this->initTemplatesAndResources();
     }
     
     protected function initTemplatesAndResources()
     {
-	$this->generateFileFromTemplate('Resources/view/Webpage/baseWebpage.html.twig',"src/{$this->name->camelcased()}/WebpageBundle/Resources",array('bundlename' => 'Webpage'));
-	$this->generateFileFromTemplate('Resources/view/Admin/baseAdmin.html.twig',"src/{$this->name->camelcased()}/AdminBundle/Resources",array('bundlename' => 'Admin'));
+	$path = $this->getContainer()->get('kernel')->locateResource('@c33sCoreBundle/Resources/views/Command/InitCmsCommand/');
+	$directoryFinder = new Finder();
+	$directoryFinder
+	    ->directories()
+	    ->in($path)
+	    ->depth('== 0')	
+	;
+	
+	$bundleNames = array();
+	foreach ($directoryFinder as $dir)
+	{
+	    $bundleNames[] =  $dir->getFilename();
+	}
+	
+	
+	foreach ($bundleNames as $bundleName)
+	{
+	    $path = $this->getContainer()->get('kernel')->locateResource('@c33sCoreBundle/Resources/views/Command/InitCmsCommand/'.$bundleName);
+	    $finder = new Finder();
+	    $finder->files()->in($path);
+	    foreach ($finder as $file) 
+	    {
+		if ($bundleName == 'General')
+		{
+		    $bundlename = '';
+		    $targetDirectory = $file->getRelativePath();
+		}
+		else
+		{
+		    $bundlename = $bundleName;
+		    $targetDirectory = "src/{$this->name->camelcased()}/${bundlename}Bundle/".$file->getRelativePath();
+		}
+		$currentFile = $bundleName.'/'.$file->getRelativePathname();
+		
+		$this->generateFileFromTemplate($currentFile,$targetDirectory,array('bundlename' => $bundlename));
+	    }
+	}
     }
-    
-    protected function initStaticPageController()
-    {
-	$this->generateFileFromTemplate('Controller/StaticPageController.php',"src/{$this->name->camelcased()}/WebpageBundle/Controller",array('bundlename' => 'Webpage'));
-	$this->generateFileFromTemplate('Controller/StaticPageController.php',"src/{$this->name->camelcased()}/AdminpageBundle/Controller",array('bundlename' => 'Admin'));
-    }
-    
+
     protected function initNameHelper($name)
     {
 	$inflector = $this->getContainer()->get('c33s_core.inflector');
 	$this->name = new NameHelper($name, $inflector);
     }
-    
-    protected function generateRouting()
-    {
-	$this->generateFileFromTemplate('app/config/routing.yml');
-	$this->generateFileFromTemplate('app/config/routing_app.yml');
-	$this->generateFileFromTemplate('app/config/security.yml');
-    }
-    
-    
+        
     protected function generatePredifinedBundles()
     {
 	foreach ($this->bundles as $bundle)
@@ -131,18 +161,21 @@ class InitCmsCommand extends ContainerAwareCommand
     protected function generateFileFromTemplate($file, $targetDirectory = null, $parameters = array())
     {
 	$fileParts = pathinfo($file);
+	//var_dump($file,$fileParts);
+	//exit;
 	
 	$parameters['name'] = $this->name;
+	$parameters['asseticBundles'] = $this->asseticBundles;
 	
-	$content = $this->getContainer()->get('templating')->render("c33sCoreBundle:Command/InitCmsCommand/${fileParts['dirname']}:${fileParts['basename']}.twig", $parameters);
+	$content = $this->getContainer()->get('templating')->render("c33sCoreBundle:Command/InitCmsCommand/${fileParts['dirname']}:${fileParts['basename']}", $parameters);
 	
 	if ($targetDirectory)
 	{
-	    $targetFile = $this->getContainer()->get('kernel')->getRootDir() . '/../'.$targetDirectory.DIRECTORY_SEPARATOR.$fileParts['basename'];
+	    $targetFile = $this->getContainer()->get('kernel')->getRootDir() . '/../'.$targetDirectory.DIRECTORY_SEPARATOR.$fileParts['filename'];
 	}
 	else
 	{
-	    $targetFile = $this->getContainer()->get('kernel')->getRootDir() . '/../'.$file;
+	    $targetFile = $this->getContainer()->get('kernel')->getRootDir() . '/../'.$fileParts['dirname'].'/'.$fileParts['filename'];
 	}
 	    
 	
